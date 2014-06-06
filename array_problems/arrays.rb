380819939fe49f6929db80b7eaf1458a328fec4c
Mark Ellis <ellismarkf@gmail.com>
2014-06-06 11:01:01 -0500

    return only element if arrary has 1 element

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,6 +3,14 @@ require 'pry-byebug'
 
 module ArrayUtil
   def self.max(array)
+    if array.length == 0
+      largest = nil
+    else
+      array.each do |number|
+        largest = number
+      end
+    end
+    largest
   end
 
   def self.middle_element(array)
